0743b7194d2cea556149173045b9040d45c85b18
Viki Bugai <vik-onegina@yandex.ru>
2022-11-17 22:52:19 +0300

    1

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupOpenButtonElement = document.querySelector('.profile__edit');
+
+const openPopup = function () {
+  popupElement.classList.add('popup_opened');
+  profileName.textContent = userName.value;
+  profileInfo.textContent = userInfo.value;
+}
+
+const closePopup = function () {
+  popupElement.classList.remove('popup_opened');
+}
+
+const formElement = document.querySelector('.popup__form');
+const userName = document.querySelector('.popup__input_type_name');
+const userInfo = document.querySelector('.popup__input_type_info');
+const profileName = document.querySelector('.profile__info_name');
+const profileInfo = document.querySelector('.profile__info_about-me');
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = userName.value;
+  profileInfo.textContent = userInfo.value;
+}
+
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
